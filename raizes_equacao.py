9ba3d59ef6ccad2093c943d185a1d268580d369c
geovanips <geovanips@outlook.com>
2018-03-02 19:43:14 -0300

    Update raizes_equacao.py

--- raizes_equacao.py
+++ raizes_equacao.py
@@ -3,7 +3,10 @@ import math
 a = int(input("Digite A: "))
 b = int(input("Digite B: "))
 c = int(input("Digite C: "))
-delta = int(((b**2) + (4 * a * c)))
+#Calcula o delta
+delta = int(((b**2) - (4 * a * c)))
+#Calcula o x1
 x1 = int((((-b) + (math.sqrt(delta)))/(a*2)))
+#Calcula o x2
 x2 = int((((-b) - (math.sqrt(delta)))/(a*2)))
 print("As raizes são x1: " + str(x1) +" e x2: " + str(x2))
