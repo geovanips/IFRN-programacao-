d16a9398f2264d48f31735eaeabbc7eb3d9ee468
geovanips <geovanips@outlook.com>
2018-08-17 21:49:11 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -1,7 +1,10 @@
-n=-1
-while (n > 12 or n < 0):
+while True:
   n=int(input("Digite um numero inteiro:"))
+  if 0 <= n <= 12:
+    break
+  else:
+    print("Digite entre 0 e 12")
 fatorial=1
 for i in range(n,1,-1):
-  fatorial=fatorial*i
-print(fatorial)
+  fatorial*=i
+print("{}! = {}".format(n, fatorial))
