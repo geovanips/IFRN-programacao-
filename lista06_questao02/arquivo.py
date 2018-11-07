02590802bbfa715928f1ea18638c1b4c053a2c9f
geovanips <geovanips@outlook.com>
2018-11-07 21:27:42 -0200

    Adicionado a função socket cliente

--- lista06_questao02/arquivo.py
+++ lista06_questao02/arquivo.py
@@ -1,5 +1,12 @@
+import socket
 def salvar_produtos(nome, preco, preco2):
     arquivo = open("quantidade.txt", "a", encoding="utf-8")
     arquivo.write("{};{};{}\n".format(nome, preco, preco2))
     arquivo.close()
 
+def inicia_conexao():
+    ip = "127.0.0.1"
+    porta = 8080
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_cliente.connect((ip, porta))
+    return socket_cliente
