85c257bca5292adb1d7e613e6a019c89f2c4da71
geovanips <geovanips@outlook.com>
2018-11-07 21:27:10 -0200

    Acionando a conexao atraves do arquivo.py

--- lista06_questao02/cliente.py
+++ lista06_questao02/cliente.py
@@ -1,23 +1,17 @@
-import socket
 import struct
 import comum
+import arquivo
 #Sockets TCP
-ip = "127.0.0.1"
-porta = 8080
-
-#cria o socket TCP
-socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#informa ao cliente socket o ip e porta para conectar ao servidor
-socket_cliente.connect((ip, porta))
 
 quantidade = int(input("Digite a quantidade de produtos a ser cadastrados: "))
 
 for i in range(1, quantidade + 1): #criando listas com produtos e preços nas duas lojas
+    cliente = arquivo.inicia_conexao()
     nome = input("Digite o nome do produto {}" .format(i))
     preco = float(input("Digite o preço do produto {} na loja 1: " .format(i)))
     preco2 = float(input("Digite o preço do produto {} na loja 2: ".format(i)))
     dados = struct.pack(comum.P_NUM, nome.encode(comum.ENCODING), preco, preco2)
     # envia os dados para o servidor
-    socket_cliente.sendall(dados)
+    cliente.sendall(dados)
+    cliente.close()
 #encerra a conexao
-socket_cliente.close()
