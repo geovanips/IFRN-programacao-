24522dc163ca7abf5e136dc5f6dccc388fa97925
geovanips <geovanips@outlook.com>
2018-11-25 20:51:37 -0300

    Create script.sql

---
+++ lista07/questao02/script.sql
@@ -0,0 +1,9 @@
+create table aluno(
+    id integer primary key,
+    nome text not null,
+    curso text not null,
+    nota1 float not null,
+    nota2 float not null,
+    nota3 float not null,
+    nota4 float not null
+);
